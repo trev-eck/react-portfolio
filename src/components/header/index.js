100b3f7ddfff4108759acda7b50c7b02b54113ed
Trevor Eckberg <trevor.eckberg@gmail.com>
2021-05-23 10:11:29 -0700

    initial commit with some file structures created

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./style.css"
+
+export default function Header() {
+    return (
+        <div>
+            <h1>Trevor Eckberg's Portfolio</h1>
+      <nav class="navigation">
+        <ul>
+          <li><a href="#about-me">About Me</a></li>
+          <li><a href="#work">Work</a></li>
+          <li><a href="#contact-me">Contact Me</a></li>
+          <li><a href="#resume">Resume</a></li>
+        </ul>
+        </nav>
+        </div>
+    )
+}
